c525ed3a6f1429bae8ef819f34202f34c82ff351
adit <aditpanchal102@gmail.com>
2022-06-09 22:43:50 +0530

    quick view added

---
+++ quickviewmodal.php
@@ -0,0 +1,118 @@
+<!-- Quick View -->
+<div class="modal quickview-wrapper">
+                                <div class="quickview">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <span class="close-qv">
+                                                <i class="flaticon-close"></i>
+                                            </span>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <!-- Product View Slider -->
+                                            <div class="quickview-slider">
+                                                <div class="slider-for">
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b1.jpg" alt="Thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b2.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b3.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b4.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b5.jpg" alt="Thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b1.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b2.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b3.jpg" alt="thumb">
+                                                    </div>
+                                                </div>
+
+                                                <div class="slider-nav">
+
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b1.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b2.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b3.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <div class="">
+                                                            <img src="media/images/product/single/b4.jpg" alt="Thumb">
+                                                        </div>
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b5.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b1.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b2.jpg" alt="thumb">
+                                                    </div>
+                                                    <div class="">
+                                                        <img src="media/images/product/single/b3.jpg" alt="thumb">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- /.quickview-slider -->
+                                        </div>
+                                        <!-- /.col-xl-6 -->
+
+                                        <div class="col-md-6">
+                                            <div class="product-details">
+                                                <h5 class="pro-title"><a href="#"><?= $product_name ?></a></h5>
+                                                <span class="price">Price : <?= $product_price ?></span>
+                                                <div class="size-variation">
+                                                    <span>size :</span>
+                                                    <select name="size-value">
+                                                        <option value="">1</option>
+                                                        <option value="">2</option>
+                                                        <option value="">3</option>
+                                                        <option value="">4</option>
+                                                        <option value="">5</option>
+                                                    </select>
+                                                </div>
+                                                <div class="color-variation">
+                                                    <span>color :</span>
+                                                    <ul>
+                                                        <li><i class="fas fa-circle"></i></li>
+                                                        <li><i class="fas fa-circle"></i></li>
+                                                        <li><i class="fas fa-circle"></i></li>
+                                                        <li><i class="fas fa-circle"></i></li>
+                                                    </ul>
+                                                </div>
+
+                                                <div class="add-tocart-wrap">
+                                                    <!--PRODUCT INCREASE BUTTON START-->
+                                                    <div class="cart-plus-minus-button">
+                                                        <input type="text" value="1" name="qtybutton" class="cart-plus-minus">
+                                                    </div>
+                                                    <a href="#" class="add-to-cart"><i class="flaticon-shopping-purse-icon"></i>Add to
+                                                        Cart</a>
+                                                    <!-- <a href="#"><i class="flaticon-valentines-heart"></i></a> -->
+                                                </div>
+                                                <p>But I must explain to you how all this mistaken idedenounc pleasure and praisi pain was
+                                                    born and I will give you a complete accosystem, and expound the actu teachings of the
+                                                    great explore tmaster-builder of human happiness. No one rejects, dislikes,
+                                                    or avoids.</p>
+                                            </div>
+                                            <!-- /.product-details -->
+                                        </div>
+                                        <!-- /.col-xl-6 -->
+                                    </div>
+                                    <!-- /.row -->
+                                </div>
+                            </div>
